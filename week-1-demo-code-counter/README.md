c3fc7d920e27ac9a494d22e74d14363430c34b51
Dan Breczinski <pt2323@gmail.com>
2025-02-01 09:05:44 +0100

    add week 1 counter demo

---
+++ week-1-demo-code-counter/README.md
@@ -0,0 +1,13 @@
+# week-1-demo-code-counter
+
+## About
+
+This project contains a simple counter app.
+
+Run the following to get started:
+
+`npm run install`
+
+`npm run dev`
+
+The main file is: `src/App.vue`
---
+++ week-1-demo-code-counter/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  computed: {
+    double() {
+      return this.count * 2;
+    },
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    currentTime() {
+      return new Date();
+    },
+  },
+  mounted() {
+    console.log(`The initial count is ${this.count}.`);
+  },
+};
+</script>
+<template>
+  <button @click="increment">Count is: {{ count }}</button>
+  <p>Twice the count is: {{ double }}</p>
+  <p>The current date and time are: {{ currentTime() }}</p>
+</template>
+<style lang="css" scoped>
+button {
+  background-color: blue;
+  color: white;
+}
+</style>
